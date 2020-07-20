c59bc580dfd3121688a340f99d24e7eb1c8c06d5
Charles Tong <charlie2020@g.ucla.edu>
2020-07-20 14:23:19 -0700

    first commit

---
+++ VoiceMail/VoiceMail.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// VoiceMail interface
+
+#include <string>
+
+class VoiceMail
+{
+public:
+	void record(std::string msg);
+	void erase();
+	void playback() const;
+private:
+	std::string m_message;
+};
